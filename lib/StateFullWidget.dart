ce1c6882e60d09ba34bed6c42a4474db8213d4cb
orionsolex <orionsolex@gmail.com>
2021-12-16 01:48:38 +0300

    statefullApp

---
+++ lib/StateFullWidget.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(myStatefullWidget());
+
+class myStatefullWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _myFirstAppState(); // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _myFirstAppState extends State<myStatefullWidget> {
+  late bool _loading;
+  late double _progressValue;
+  late var _icon;
+
+@override
+  void initState() {
+    // TODO: implement initState
+  _loading = false;
+  _progressValue = 0.0;
+  _icon=Icons.cloud_download;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          setState(() {
+            _loading = !_loading;
+            _updateProgress();
+          });
+        },
+          child: Icon(_icon),),
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text("Хромая кляча"),
+          centerTitle: true,
+        ),
+        body: Center(child: Container(
+          padding: EdgeInsets.all(16),
+          child: _loading
+              ? Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              LinearProgressIndicator(
+                value: _progressValue,
+              ),
+              Text("${(_progressValue * 100).round()}%",
+                  style: TextStyle(color: Colors.white, fontSize: 20)),
+            ],
+
+          )
+              : Text("Press button to download",
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+        ),
+        ),
+
+      ),); // TODO: implement build
+    throw
+    UnimplementedError
+      (
+    );
+  }
+
+  void _updateProgress(){
+  const oneSec=const Duration(seconds: 1);
+  Timer.periodic(oneSec, (timer) {
+    setState(() {
+      _progressValue+=0.1;
+      ((_progressValue*100)%2)==0?_icon=Icons.swap_vertical_circle_outlined:_icon=Icons.swap_horizontal_circle;
+      if(_progressValue.toStringAsFixed(1)=="1.0"){
+        _loading=false;
+        timer.cancel();
+        _icon=Icons.cloud_done;
+        _progressValue=0.0;
+        return;
+      }
+    });
+  });
+  }
+}
