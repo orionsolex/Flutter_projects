bfeb7e2996c51d988f38c09a5ca2b2a88e616d7d
orionsolex <orionsolex@gmail.com>
2021-12-16 01:52:34 +0300

    remove warnings

--- lib/StateFullWidget.dart
+++ lib/StateFullWidget.dart
@@ -1,29 +1,26 @@
 import 'dart:async';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(myStatefullWidget());
+void main() => runApp(MyStatefullWidget());
 
-class myStatefullWidget extends StatefulWidget {
+class MyStatefullWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _myFirstAppState(); // TODO: implement createState
-    throw UnimplementedError();
+    return _MyFirstAppState(); // TODO: implement createState
   }
 }
 
-class _myFirstAppState extends State<myStatefullWidget> {
+class _MyFirstAppState extends State<MyStatefullWidget> {
   late bool _loading;
   late double _progressValue;
   late var _icon;
 
-@override
+  @override
   void initState() {
     // TODO: implement initState
-  _loading = false;
-  _progressValue = 0.0;
-  _icon=Icons.cloud_download;
+    _loading = false;
+    _progressValue = 0.0;
+    _icon = Icons.cloud_download;
     super.initState();
   }
 
@@ -31,58 +28,58 @@ class _myFirstAppState extends State<myStatefullWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          setState(() {
-            _loading = !_loading;
-            _updateProgress();
-          });
-        },
-          child: Icon(_icon),),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: Icon(_icon),
+        ),
         backgroundColor: Colors.indigo,
         appBar: AppBar(
           title: Text("Хромая кляча"),
           centerTitle: true,
         ),
-        body: Center(child: Container(
-          padding: EdgeInsets.all(16),
-          child: _loading
-              ? Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              LinearProgressIndicator(
-                value: _progressValue,
-              ),
-              Text("${(_progressValue * 100).round()}%",
-                  style: TextStyle(color: Colors.white, fontSize: 20)),
-            ],
-
-          )
-              : Text("Press button to download",
-              style: TextStyle(color: Colors.white, fontSize: 20)),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      LinearProgressIndicator(
+                        value: _progressValue,
+                      ),
+                      Text("${(_progressValue * 100).round()}%",
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ],
+                  )
+                : Text("Press button to download",
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+          ),
         ),
-        ),
-
-      ),); // TODO: implement build
-    throw
-    UnimplementedError
-      (
-    );
+      ),
+    ); // TODO: implement build
   }
 
-  void _updateProgress(){
-  const oneSec=const Duration(seconds: 1);
-  Timer.periodic(oneSec, (timer) {
-    setState(() {
-      _progressValue+=0.1;
-      ((_progressValue*100)%2)==0?_icon=Icons.swap_vertical_circle_outlined:_icon=Icons.swap_horizontal_circle;
-      if(_progressValue.toStringAsFixed(1)=="1.0"){
-        _loading=false;
-        timer.cancel();
-        _icon=Icons.cloud_done;
-        _progressValue=0.0;
-        return;
-      }
+  void _updateProgress() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (timer) {
+      setState(() {
+        _progressValue += 0.1;
+        ((_progressValue * 100) % 2) == 0
+            ? _icon = Icons.swap_vertical_circle_outlined
+            : _icon = Icons.swap_horizontal_circle;
+        if (_progressValue.toStringAsFixed(1) == "1.0") {
+          _loading = false;
+          timer.cancel();
+          _icon = Icons.cloud_done;
+          _progressValue = 0.0;
+          return;
+        }
+      });
     });
-  });
   }
 }
