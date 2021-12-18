daa0c6407b767c2b9e80a8e04c2d45ad5bf1189b
orionsolex <orionsolex@gmail.com>
2021-12-18 17:50:20 +0300

    Add fonts asset

--- lib/assetExample.dart
+++ lib/assetExample.dart
@@ -6,10 +6,11 @@ class MyStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: "IndieFlower"),
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text("Дырявый кошель"),
+          title: Text("Wikness pocket"),
           centerTitle: true,
         ),
         body: Center(
@@ -17,7 +18,15 @@ class MyStatelessWidget extends StatelessWidget {
           fit: StackFit.expand,
           children: <Widget>[
             Image(image: AssetImage("assets/images/bg.jpg")),
-            Image.asset("assets/icons/icon.png")
+            Image.asset("assets/icons/icon.png"),
+            Positioned(
+                top: 16,
+                left: 115,
+                child: Text(
+                  "My custom font",
+                  style: TextStyle(
+                      fontSize: 30, color: Colors.white,fontFamily: "IndieFlower"),
+                ))
           ],
         )),
       ),
