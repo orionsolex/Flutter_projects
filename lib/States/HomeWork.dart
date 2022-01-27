58a7903d8b335b5325393543197c9f3d49a8a483
orionsolex <orionsolex@gmail.com>
2022-01-28 01:00:52 +0300

    origin

--- lib/States/HomeWork.dart
+++ lib/States/HomeWork.dart
@@ -1,3 +1,5 @@
+// @dart=2.7
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:randomizer/randomizer.dart';
@@ -5,51 +7,14 @@ import 'package:randomizer/randomizer.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'HomeWork Provider',
-        home: ChangeNotifierProvider<MyProvider>(create: (BuildContext context) =>MyProvider(),
-        child:Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text(
-              "Home Work Provider",
-              style: TextStyle(color: Colors.amberAccent),
-            ),
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              MyContainer(),
-              MySwitch()
-            ],
-          )
-      )        ),
-        // home: MultiProvider(
-        //   providers: [
-        //     ChangeNotifierProvider<MyProvider>.value(value: MyProvider()),
-        //     ChangeNotifierProvider<MyProvider>(create: (context)=>MyProvider(),)
-        //   ] ,
-        //   child: Scaffold(
-        //       appBar: AppBar(
-        //         centerTitle: true,
-        //         title: Text(
-        //           "Home Work Provider",
-        //           style: TextStyle(color: Colors.amberAccent),
-        //         ),
-        //       ),
-        //       body: Column(
-        //         mainAxisAlignment: MainAxisAlignment.center,
-        //         children: <Widget>[
-        //           MyContainer(),
-        //           MySwitch()
-        //         ],
-        //       )
-        //   ),
-        // )
+      title: 'HomeWork Provider',
+      home: ChangeNotifierProvider<MyProvider>(
+          create: (BuildContext context) => MyProvider(), child: MyContainer()),
     );
   }
 }
@@ -59,46 +24,66 @@ class MyContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(seconds: 1),
-      curve: Curves.ease,
-      width: selected ? 200.0 : 100.0,
-      height: selected ? 100.0 : 200.0,
-      color: selected ? Colors.red : Colors.deepPurpleAccent,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 50,
+    MyProvider _state = Provider.of<MyProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Home Work Provider",
+          style: TextStyle(color: _state._color2),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 100,),
+            AnimatedContainer(
+              duration: Duration(seconds: 1),
+              curve: Curves.ease,
+              width: 100.0,
+              height: 100.0,
+              color: _state._color,
             ),
-          ]),
+            MySwitch()
+          ],
+        ),
+      ),
     );
   }
 }
 
 class MySwitch extends StatelessWidget {
-  bool stat
-    MyProvider _state = Provider.of<MyProvider>(context);
   @override
   Widget build(BuildContext context) {
-    return Switch(value: _state._switchState, onChanged: (true){
+    MyProvider _state = Provider.of<MyProvider>(context);
+    return Switch(
+        value: _state._switchState,
+        onChanged: (value) {
+          _state._randomizecolor();
+          _state.switchState=value;
 
-    });
+        });
   }
-
 }
 
 class MyProvider extends ChangeNotifier {
   var _color;
-  bool _switchState=false;
-  Randomizer randomizer = Randomizer();
+  var _color2;
 
-  String get color => _color;
+  get color2 => _color2;
+  get color => _color;
+
+  bool _switchState = false;
+
+  set switchState(bool value) {
+    _switchState = value;
+  }
+
+  Randomizer randomizer = Randomizer();
 
   void _randomizecolor() {
     _color = randomizer.getrandomcolor();
+    _color2=randomizer.getrandomcolor();
     notifyListeners();
   }
-
-
 }
