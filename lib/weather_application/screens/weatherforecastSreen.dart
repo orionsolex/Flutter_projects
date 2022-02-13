d68e99cc412b47ec50f5e29a22a07346eec216f2
orionsolex <orionsolex@gmail.com>
2022-02-13 17:09:41 +0300

    origin

--- lib/weather_application/screens/weatherforecastSreen.dart
+++ lib/weather_application/screens/weatherforecastSreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_projects/weather_application/api/weather_api.dart';
 import 'package:flutter_projects/weather_application/model/WeatherForecastDaily.dart';
+import 'package:flutter_projects/weather_application/screens/city_screen.dart';
 import 'package:flutter_projects/weather_application/widgets/city_view.dart';
 import 'package:flutter_projects/weather_application/widgets/daily_forecast.dart';
 import 'package:flutter_projects/weather_application/widgets/detail_view.dart';
@@ -8,21 +9,19 @@ import 'package:flutter_projects/weather_application/widgets/temp_view.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class WeatherForecastScreen extends StatefulWidget {
-
   @override
   _WeatherForecastScreenState createState() => _WeatherForecastScreenState();
 }
 
 class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
-
   late Future<WeatherForecast> forecastObject;
-  String cityName='Moscow';
-   String weather='';
+  String _cityName = 'Moscow';
+  String weather = '';
 
   @override
   void initState() {
-         super.initState();
-    forecastObject=WeatherApi().fetchWeatherForecastByCity(cityName);
+    super.initState();
+    forecastObject = WeatherApi().fetchWeatherForecastByCity(_cityName);
     // forecastObject.then((value) {
     //         print(value.list![0].weather![0].main);
     // }
@@ -38,10 +37,24 @@ class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
         centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.my_location),
-          onPressed: (){},
+          onPressed: () {},
         ),
         actions: <Widget>[
-          IconButton(onPressed: (){}, icon: Icon(Icons.location_city))
+          IconButton(
+              onPressed: () async {
+                var tappedName = await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return CityScreen();
+                }));
+                if (tappedName != null) {
+                  setState(() {
+                    _cityName = tappedName;
+                    forecastObject =
+                        WeatherApi().fetchWeatherForecastByCity(_cityName);
+                  });
+                }
+              },
+              icon: Icon(Icons.location_city))
         ],
       ),
       body: ListView(
@@ -49,21 +62,29 @@ class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
           Container(
             child: FutureBuilder<WeatherForecast>(
               future: forecastObject,
-              builder: (context,snapshot){
-                if(snapshot.hasData){
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
                   return Column(
                     children: <Widget>[
-                      SizedBox(height: 50,),
+                      SizedBox(
+                        height: 50,
+                      ),
                       CityView(snapshot: snapshot),
-                      SizedBox(height: 50,),
+                      SizedBox(
+                        height: 50,
+                      ),
                       TempView(snapshot: snapshot),
-                      SizedBox(height: 50,),
+                      SizedBox(
+                        height: 50,
+                      ),
                       DetailView(snapshot: snapshot),
-                      SizedBox(height: 50,),
+                      SizedBox(
+                        height: 50,
+                      ),
                       ButtonListView(snapshot: snapshot),
                     ],
                   );
-                }else{
+                } else {
                   return Center(
                     child: SpinKitDoubleBounce(
                       color: Colors.black,
